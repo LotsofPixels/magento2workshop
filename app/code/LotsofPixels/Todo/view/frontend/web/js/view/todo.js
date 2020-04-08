1d5c8c853901e3013ba0d51b425bafb7d0dd4dd6
Rene Donkers <rene@lotsofpixels.nl>
2020-04-08 05:40:51 +0000

    Lesson 16

--- app/code/LotsofPixels/Todo/view/frontend/web/js/view/todo.js
+++ app/code/LotsofPixels/Todo/view/frontend/web/js/view/todo.js
@@ -1,5 +1,14 @@
 define(['uiComponent'], function (Component) {
     'use strict';
 
-return Component.extend({});
+    return Component.extend({
+        defaults: {
+            tasks: [
+             {label: "Taak 1"},
+             {label: "Taak 2"},
+             {label: "Taak 3"},
+             {label: "Taak 4"},
+          ]
+    }
+    });
 });
